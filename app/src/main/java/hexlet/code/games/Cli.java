7458ebb7f4c4b9341b9bc75a523a197bd0df1848
Barlog7 <barlog7@yandex.ru>
2023-10-10 17:06:47 +0300

    fix hello

--- app/src/main/java/hexlet/code/games/Cli.java
+++ app/src/main/java/hexlet/code/games/Cli.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Cli {
     public static String hello(Scanner sc) {
 
-        System.out.print("What is your name? ");
+        System.out.print("May I have your name? ");
         String userName = sc.next();
         System.out.println("Hello, " + userName + "!");
 
