2764b06b2a71c88cc6c0bb709a207e7abdc4d3c2
Barlog7 <barlog7@yandex.ru>
2023-10-16 13:33:22 +0300

    3 point fix Cli class

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,15 +2,12 @@ package hexlet.code;
 
 import java.util.Scanner;
 public class Cli {
-    public static String greetings() {
+    public static void greetings() {
 
         Scanner scannerApp = new Scanner(System.in);
         System.out.print("May I have your name? ");
         String userName = scannerApp.next();
         System.out.println("Hello, " + userName + "!");
         scannerApp.close();
-
-
-        return userName;
     }
 }
